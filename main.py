22055899820c8467b07807dc783b40ad29cdad7e
muysengly <muysengly@gmail.com>
2023-05-26 06:59:13 +0900

    z

--- main.py
+++ main.py
@@ -7,4 +7,27 @@ def hello():
     return "Hello, Flask!"
 
 if __name__ == '__main__':
-    app.run()
+    # Use Gunicorn as the WSGI server
+    import os
+    from gunicorn.app.base import BaseApplication
+
+    class FlaskApplication(BaseApplication):
+        def __init__(self, app, options=None):
+            self.options = options or {}
+            self.application = app
+            super().__init__()
+
+        def load_config(self):
+            config = {key: value for key, value in self.options.items() if key in self.cfg.settings and value is not None}
+            for key, value in config.items():
+                self.cfg.set(key.lower(), value)
+
+        def load(self):
+            return self.application
+
+    options = {
+        'bind': '0.0.0.0:8000',
+        'workers': os.cpu_count() * 2 + 1
+    }
+
+    FlaskApplication(app, options).run()
