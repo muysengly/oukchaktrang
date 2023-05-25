8517456a2bbd7ca5678ba53a6f471154994b6476
muysengly <muysengly@gmail.com>
2023-05-26 06:56:06 +0900

    z

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI
+from flask import Flask
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.route('/')
+def hello():
+    return "Hello, Flask!"
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == '__main__':
+    app.run()
